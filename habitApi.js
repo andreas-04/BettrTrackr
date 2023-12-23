6c594c37ce1545452072b64d9b321d9919d8988f
Andreas <andreasneacsu@gmail.com>
2023-12-23 00:29:27 -0800

    create instance

---
+++ habitApi.js
@@ -0,0 +1,24 @@
+import axios from 'axios';
+
+const apiClient = axios.create({
+  baseURL: 'http://localhost:8000/api',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  withCredentials: true,
+});
+
+export default {
+  getHabitTrackerTasks(habitId) {
+    return apiClient.get(`/habitTrackers/${habitId}/tasks/`);
+  },
+  postHabitTracker(userID){
+    return apiClient.post(`/habitTrackers/`, userID)
+},
+  deleteTask(taskId) {
+    return apiClient.delete(`/tasks/${taskId}`);
+  },
+  addTask(taskData) {
+    return apiClient.post('/tasks/', taskData);
+  }
+};
