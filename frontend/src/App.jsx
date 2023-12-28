60176c6f1adfb01a4fd3f5fc3542422534caa789
Andreas <andreasneacsu@gmail.com>
2023-12-27 19:47:47 -0800

    style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import './App.css' // Importing the CSS for the App component
 import { useCookies } from 'react-cookie'; // Importing the useCookies hook from 'react-cookie' for managing cookies
 import userApi from '../../userApi' // Importing the userApi module
 import habitApi from '../../habitApi'; // Importing the habitApi module
+import { Sheet, Typography, Button, Checkbox, Input, Box } from '@mui/joy';
 
 // Define the App component
 function App() {
@@ -74,6 +75,19 @@ function App() {
       console.error('Error getting habit ID:', error);
     }
   }
+  const handleCheckboxChange = (taskID, completed) => {
+    const updatedTask = tasks.find(task => task.id === taskID);
+  
+    updatedTask.completed = completed;
+  
+    habitApi.updateTask(taskID, updatedTask)
+      .then(() => {
+        setTasks(tasks.map(task => task.id === taskID ? updatedTask : task));
+      })
+      .catch((error) => {
+        console.error('Error updating task:', error);
+      });
+  };
   // Check if the 'userID' cookie exists 
   if (!cookies.userID) {
     // If the 'userID' cookie doesn't exist, create a new user
@@ -95,31 +109,49 @@ function App() {
 
   // Render the App component
   return (
-    <div>
-      {cookies.userID ? (
-        <>
-          <form onSubmit={handleAddTask}>
-            <input
-              type="text"
-              value={newTaskName}
-              onChange={e => setNewTaskName(e.target.value)}
-            />
-            <button type="submit">Add Task</button>
-          </form>
-          {tasks.length > 0 ? (
-            <ul>
-              {tasks.map(task => (
-                <li key={task.id}>{task.name}</li>
-              ))}
-            </ul>
-          ) : (
-            'No tasks found.'
-          )}
-        </>
-      ) : (
-        'No userID cookie found.'
-      )}
-    </div>
+    <Sheet
+      sx={{
+        mx: 4,
+        p: 3,
+        display: 'flex',
+        flexDirection: 'column',
+        gap: 2,
+        width: 500,
+        variant: "outlined",
+        '& > div': { p: 2, borderRadius: 'md', display: 'flex' },
+      }}
+    >
+      <div style={{display: 'flex', justifyContent: 'space-between'}}>
+        <Typography
+          color="neutral"
+          level="h2"
+          variant="plain"
+        >
+          Habit Tracker
+        </Typography>
+      </div>
+  
+      {tasks.map((task) => (
+        <Sheet variant="outlined" sx={{ height: '30px' }} key={task.id}>
+          <div>
+            <Checkbox overlay label={task.name} checked={task.completed} onChange={(e) => handleCheckboxChange(task.id, e.target.checked)} sx={{ width: '500px' }} />
+          </div>
+
+        </Sheet>
+      ))}
+      <Box sx={{ display: 'flex', flexDirection: 'row', gap: 2, alignItems: 'center' }}>
+        <Input
+          placeholder="New Task"
+          variant="outlined"
+          size="md"
+          value={newTaskName}
+          onChange={(e) => setNewTaskName(e.target.value)}
+        />
+        <Button startDecorator="+" onClick={handleAddTask}>
+          Task
+        </Button>
+      </Box>
+    </Sheet>
   );
 }
 
